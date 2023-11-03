2b6ecc36bf89899a20498403126f7bdf74ca4a7d
sainathhiwale <sainathhiwale2@gmail.com>
2023-11-03 18:50:12 +0530

    Initial commit

---
+++ StringDemo.dart
@@ -0,0 +1,16 @@
+
+void main(){
+
+  String name = "Sainath";
+  String address = "India";
+  num age = 20; // used to store any types of numbers
+  num height = 5.9;
+  bool isMarried = false;
+
+// printing variables value
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+}
