8a051ff3a91770348fd4f3a5f322bd4bda66371f
sainathhiwale <sainathhiwale2@gmail.com>
2024-03-29 15:20:25 +0530

    set collection sample code is added

---
+++ collection_dart/SetCollectionDemo.dart
@@ -0,0 +1,18 @@
+void main(){
+  Set<String> fruits = {"apple", "Mango", "Banana","grova"};
+  print(fruits);
+
+
+
+
+
+
+
+
+
+  //Set
+  /*Set is a unique collection of items. You cannot store duplicate values in the Set. It is unordered,
+  so it can be faster than lists while working with a large amount of data. Set is useful when you need
+  to store unique values without considering the order of the input. E.g.,
+  fruits name, months name, days name, etc. It is represented by Curley Braces{}.*/
+}
