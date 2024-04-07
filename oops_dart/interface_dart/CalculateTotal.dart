ac79f68b1f4fcc71ce46486f7fdd6c45d9133081
sainathhiwale <sainathhiwale2@gmail.com>
2024-04-07 11:57:07 +0530

    multiple inheritance is implemented through the interface sample-2

---
+++ oops_dart/interface_dart/CalculateTotal.dart
@@ -0,0 +1,31 @@
+// abstract class as interface
+abstract class CalculateTotal {
+  int total();
+}
+// abstract class as interface
+abstract class CalculateAverage {
+  double average();
+}
+// implements multiple interfaces
+class Student implements CalculateTotal, CalculateAverage {
+// properties
+  int marks1, marks2, marks3;
+// constructor
+  Student(this.marks1, this.marks2, this.marks3);
+// implementation of average()
+  @override
+  double average() {
+    return total() / 3;
+  }
+// implementation of total()
+  @override
+  int total() {
+    return marks1 + marks2 + marks3;
+  }
+}
+
+void main() {
+  Student student = Student(90, 80, 70);
+  print('Total marks: ${student.total()}');
+  print('Average marks: ${student.average()}');
+}
